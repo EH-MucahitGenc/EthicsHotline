f5f949b5f834b25507300e80c016494c355e98ad
Mücahit Genç <105339374+EH-MucahitGenc@users.noreply.github.com>
2025-11-03 14:55:21 +0300

    ok

--- EthicsHotline/obj/Debug/net8.0/EthicsHotline.AssemblyInfo.cs
+++ EthicsHotline/obj/Debug/net8.0/EthicsHotline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EthicsHotline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e00f88252b67748d5bb63346863c7e8228cb4e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2784c689cc5edfedc0420a0f694639f53e88e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("EthicsHotline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EthicsHotline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
